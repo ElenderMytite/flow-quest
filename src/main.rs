213a09fc3567cbccb8279d61f92b06d2fa8d8125
ElenderMytite <ElenderMytite>
2026-03-25 15:39:43 +0100

    filename input

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,12 @@ use std::collections::HashMap;
 use std::fs;
 use std::rc::Rc;
 fn main() {
-    let file = fs::read_to_string("test.code").expect("cannot read file");
+    let mut input: String = "".to_string() ;
+    std::io::stdin().read_line(&mut input).expect("cannot read filename");
+    let file = fs::read_to_string(format!("code/{}.fq", input.trim())).expect("cannot read file");
     let vocabulary: Vocabulary = read_json("vocabulary.json".to_string());
     let mut tokens: Vec<types::Token> = lexer::tokenize_code(file, &vocabulary.keywords);
     let tree: Rc<types::Statement> = Rc::new(parse_program(&mut tokens, &RefCell::new(FlowListener::Console)));
-    assert_eq!(tokens.len(), 0);
     let mut ir: Vec<ir::IR> = vec![];
     ir::ast_to_ir(&tree, &mut ir);
     let mut env: HashMap<usize, VarV> = HashMap::new();
