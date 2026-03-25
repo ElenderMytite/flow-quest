10bf94620a8d8c46915d0e634ae4c326e1075f6e
ElenderMytite <ElenderMytite>
2026-03-25 15:45:02 +0100

    correct import

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod lexer;
 mod parser;
 mod vm;
 mod types;
-use crate::types::VarV;
+use vm::VarV;
 use inout::{read_json, Vocabulary};
 use parser::parse_program;
 use types::FlowListener;
@@ -17,7 +17,7 @@ fn main() {
     std::io::stdin().read_line(&mut input).expect("cannot read filename");
     let file = fs::read_to_string(format!("code/{}.fq", input.trim())).expect("cannot read file");
     let vocabulary: Vocabulary = read_json("vocabulary.json".to_string());
-    let mut tokens: Vec<types::Token> = lexer::tokenize_code(file, &vocabulary.keywords);
+    let mut tokens: Vec<lexer::Token> = lexer::tokenize_code(file, &vocabulary.keywords);
     let tree: Rc<types::Statement> = Rc::new(parse_program(&mut tokens, &RefCell::new(FlowListener::Console)));
     let mut ir: Vec<ir::IR> = vec![];
     ir::ast_to_ir(&tree, &mut ir);
